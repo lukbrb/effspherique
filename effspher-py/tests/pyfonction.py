2851a63c5d31de94acd50ce85fbfd6c2f004ff79
lukbrb <lucasbarbr@outlook.com>
2023-07-19 22:01:28 +0200

    add test

---
+++ effspher-py/tests/pyfonction.py
@@ -0,0 +1,11 @@
+import unittest
+from effspherpy.linear import euler
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
