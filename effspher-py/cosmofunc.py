9fb16a6a0fcfe1f9de3fe60a983a754cfd45ad0f
lukbrb <lucasbarbr@outlook.com>
2023-07-25 17:15:29 +0200

    add age_univ and surd_mini

--- effspher-py/cosmofunc.py
+++ effspher-py/cosmofunc.py
@@ -1,24 +1,23 @@
 import numpy as np
 
-# Déclaration des variables
+
 Mp = 3.08567758 * 1e22  # Valeur d'un mégaparsec
 H0 = 7 * 1e4 / Mp  # Constante d'Hubble Actuelle
 G = 6.6742 * 1e-11  # Constante de gravitation
 rho_crit = (3 * H0 ** 2) / (8 * np.pi * G)  # Densité critique
 sig = 1  # Densité de l'univers
 milliard_annee = 3600 * 24 * 365 * 1e9
-
-
+age_univ = 2 / (3 * H0)  # L'âge de l'univers
+surd_mini = 0.001777656936645508  # Surdensité mini pour que teff <= age_univers
 # CONDITIONS INITIALES
 # On veut ti très petit devant 1/H0, or 1/H0= 4.4e17s (=13 952 308 472 ans)
 # On peut donc prendre ti=300000 ans par exemple
-# Grille de temps
 
 ti = 300000 * 365 * 24 * 3600
 tf = 1 / H0
 N = 5000
 ttab = np.linspace(ti, tf, N)
-dt = (tf - ti) / N  # DEFINITION DU PAS DE TEMPS
+dt = (tf - ti) / N
 ttab1 = ttab / milliard_annee
 
 
