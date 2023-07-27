aab6f1c353e4e91a95839e0ce93d379723c34350
lukbrb <lucasbarbr@outlook.com>
2023-07-27 22:28:24 +0200

    deplace constantes dans module cosmofunc

--- effspher-f/app/main.f90
+++ effspher-f/app/main.f90
@@ -1,24 +1,16 @@
 program main
-  use effspher_f, only: say_hello
-  use solvers, only: rk4, H
+  use solvers, only: rk2, rk4
+  use cosmofunc, only: di, ti, t_max
   implicit none
 
-  real :: di, pi, ti, t_max, dt, max_density, Gyr, Mp, H0, result
+  real :: dt = 1e-5
+  real :: max_density = 1e4
+  real, dimension(2) :: result2, result4
 
-  Mp = 3.08567758 * 1E22  ! Valeur d un mégaparsec
-  H0 = 7 * 1e4 / Mp
-  Gyr = 3600 * 24 * 365 * 1E09
-  ti = 300000 * 365 * 24 * 3600
-  ti = ti / Gyr
-  di = 0.0017104343414306644
-  pi = H(ti) * di
-  t_max = (1 / H0) / Gyr
-  dt = 1e-5
-  max_density = 1e4
-
-
-  result = rk4(di, pi, ti, t_max, dt, max_density)
-  print *, 'Résultat RK4:', result
+  result2 = rk2(di, ti, t_max, dt, max_density)
+  result4 = rk4(di, ti, t_max, dt, max_density)
+  print *, 'Résultat RK2:', result2
+  print *, 'Résultat RK4:', result4
   ! call say_hello()
 
 end program main
