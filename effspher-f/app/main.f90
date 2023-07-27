0eba726c07faf1a9a3532cb890a7b9dfc9ce22df
lukbrb <lucasbarbr@outlook.com>
2023-07-27 21:54:04 +0200

    initialisation simulation constantes

--- effspher-f/app/main.f90
+++ effspher-f/app/main.f90
@@ -1,6 +1,24 @@
 program main
   use effspher_f, only: say_hello
+  use solvers, only: rk4, H
   implicit none
 
-  call say_hello()
+  real :: di, pi, ti, t_max, dt, max_density, Gyr, Mp, H0, result
+
+  Mp = 3.08567758 * 1E22  ! Valeur d un mégaparsec
+  H0 = 7 * 1e4 / Mp
+  Gyr = 3600 * 24 * 365 * 1E09
+  ti = 300000 * 365 * 24 * 3600
+  ti = ti / Gyr
+  di = 0.0017104343414306644
+  pi = H(ti) * di
+  t_max = (1 / H0) / Gyr
+  dt = 1e-5
+  max_density = 1e4
+
+
+  result = rk4(di, pi, ti, t_max, dt, max_density)
+  print *, 'Résultat RK4:', result
+  ! call say_hello()
+
 end program main
