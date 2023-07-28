e69c31e01c4e19d90caf1e359012981faa89d62f
lukbrb <lucasbarbr@outlook.com>
2023-07-28 10:41:15 +0200

    ajout eq diff non linéaire

--- effspher-f/src/cosmofunc.f90
+++ effspher-f/src/cosmofunc.f90
@@ -2,11 +2,11 @@ module cosmofunc
     implicit none
     private
 
-    public :: H, eq_diff, di, ti, t_max
+    public :: H, eq_diff_lin, eq_diff, di, ti, t_max
     
     real, parameter :: Mp = 3.08567758 * 1E22  ! Valeur d un mégaparsec
     real, parameter :: H0 = 7 * 1e4 / Mp
-    real, parameter :: Gyr = 3600 * 24 * 365 * 1E09
+    real, parameter :: Gyr = 3600. * 24. * 365. * 1E09
     real, parameter :: ti = (300000. * 365. * 24. * 3600.) / Gyr
     real, parameter :: di = 0.0017104343414306644
     real, parameter :: t_max = (1 / H0) / Gyr
@@ -18,10 +18,14 @@ contains
         hubble_cst = 2 / (3 * t)
     end function H
 
-    function eq_diff(d, p, t) result(val_eq)
+    real function eq_diff_lin(d, p, t) result(val_eq)
         real, intent(in) :: d, p, t
-        real :: val_eq
         val_eq = -2 * H(t) * p + 1.5 * (H(t) ** 2) * d
-end function eq_diff
+    end function eq_diff_lin
+
+    real function eq_diff(d, p, t) result(val_eq)
+    real, intent(in) :: d, p, t
+    val_eq = -2 * H(t) * p + 1.5 * (H(t) ** 2) * d * (1 + d) + (4 / 3) * ((p ** 2) / (1 + d))
+    end function eq_diff
 
 end module cosmofunc
