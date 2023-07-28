d3340ab517ffb5e32842d151df2bcce727388a88
lukbrb <lucasbarbr@outlook.com>
2023-07-28 10:40:29 +0200

    ajout Euler, et modif surdensité init dans arg fonction

--- effspher-f/app/main.f90
+++ effspher-f/app/main.f90
@@ -1,16 +1,18 @@
 program main
-  use solvers, only: rk2, rk4
+  use solvers, only: rk2, rk4, euler
   use cosmofunc, only: di, ti, t_max
   implicit none
 
   real :: dt = 1e-5
   real :: max_density = 1e4
-  real, dimension(2) :: result2, result4
+  real, dimension(2) :: resulte, result2, result4
 
-  result2 = rk2(di, ti, t_max, dt, max_density)
-  result4 = rk4(di, ti, t_max, dt, max_density)
-  print *, 'Résultat RK2:', result2
-  print *, 'Résultat RK4:', result4
+  resulte = euler(4. * di, ti, t_max, dt, max_density)
+  result2 = rk2(4. * di, ti, t_max, dt, max_density)
+  result4 = rk4(4. * di, ti, t_max, dt, max_density)
+  print *, 'Résultat Euler explicit :', resulte
+  print *, 'Résultat RK2 :', result2
+  print *, 'Résultat RK4 :', result4
   ! call say_hello()
 
 end program main
