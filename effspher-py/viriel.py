bea19ed8d26f79fc3482b38ffea2db5ba397b909
lukbrb <lucasbarbr@outlook.com>
2023-07-31 21:03:15 +0200

    modify dt val and arange time units

--- effspher-py/viriel.py
+++ effspher-py/viriel.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from cosmofunc import rho_m, G, a, H, dt, milliard_annee, surd_mini, eq_diff, tf, ti
+from cosmofunc import rho_m, G, a, H, milliard_annee, surd_mini, eq_diff, tf, ti
 from solvers import rk4
 
-PLOT = False
+PLOT = True
 tf /= milliard_annee
 ti /= milliard_annee
 
@@ -32,30 +32,10 @@ def t_viriel(borne_min, borne_max, tolerance, r, r_vir):
 
 
 # Fonction qui fait évoluer la surdensité après la surdensité viriel
-
-# TODO: Vectoriser cette fonction
-# def surdensite(index):
-#     for i in range(0, index):
-#         delta[i] = delta[i]
-#     for i in range(index, len(delta)):
-#         delta[i] = rho_vir / rho_m(ttab[i]) - 1
-#     return delta
 def surdensite(_delta, index):
     _delta[index:] = rho_vir / rho_m(ttab[index:]) - 1
     return _delta
 
-
-# Vitesse calculée analytiquement
-# TODO: Probablement vectorisable
-# def vitesse(r_final):
-#     r = r_final
-#     _v = np.zeros(len(r))
-#     v_test = np.zeros(len(r))
-#     for i in range(0, len(r)):
-#         t = ttab[i]
-#         _v[i] = H(t) * r[i] * (1 - (dv[i] * r[i] ** 3) / (9 * Masse * G * t))
-#         v_test[i] = H(t) * r[i] - (r[i] * dv[i]) / (3 * (1 + delta[i]))
-#     return _v, v_test
 
 def vitesse(r_final):
     r = r_final
@@ -64,39 +44,28 @@ def vitesse(r_final):
     return _v, v_test
 
 
-# def Vitesse_alter(R):  # VITESSE CALCULEE DIRECTEMENT A PARTIR DE R ET dt
-#     V = np.zeros(len(R))
-#     for i in range(0, len(R) - 1):
-#         dR = R[i + 1] - R[i]
-#         V[i] = dR / dt
-#     return V
-#
-def Vitesse_alter(r):  # VITESSE CALCULEE DIRECTEMENT A PARTIR DE R ET dt
+def Vitesse_alter(r, dt=1e-5):  # VITESSE CALCULEE DIRECTEMENT A PARTIR DE R ET dt
     dr = np.diff(r)
     _v = dr / dt
     return np.pad(_v, (0, 1), mode='constant')  # numpy pad pour avoir len(v) == len(r)
 
 
-# Formule pour l'énergie cinétique
-# def Ecin(Rcourt, vitesse):
-#     R = Rcourt
-#     Ecin = np.zeros(len(R))
-#     for i in range(0, len(R)):
-#         v = vitesse[i]
-#         Ecin[i] = (3 * Masse * v ** 2) / 10
-#     return Ecin
-
-def Ecin(_v):
+def energie_cin(_v):
     return (3 * Masse * _v ** 2) / 10
 
 
+def energie_pot(r_vir):
+    return -(3 * G * Masse ** 2) / (5 * r_vir)
 # Calcul du rayon viriel en prenant la moitié de son rayon max (méthode 1)
+
 
 R = ((3 * Masse) / (4 * np.pi * rho_m(ttab) * (1 + delta))) ** (1 / 3)
 Rmax, imax = R.max(), R.argmax()
 tmax = ttab[imax]
-Rvir = Rmax * 0.5
+Rvir = Rmax / 2
 
+print("Rayon maximum:", Rmax, "atteint à:", tmax)
+print("Le rayon viriel est:", Rvir)
 # On ajuste les valeurs de la surdensité une fois R_vir atteint
 
 tvir1 = t_viriel(0, len(delta), 1e-6, R, Rvir)
@@ -113,13 +82,13 @@ R_final = ((3 * Masse) / (4 * np.pi * rho_m(ttab) * (1 + delta_final))) ** (1 / 
 
 # Calcul des vitesses via méthodes numériques et formule analytique
 v, vtest = vitesse(R_final)
-Vitesse2 = Vitesse_alter(R_final)
+v_num = Vitesse_alter(R_final)
 
 # Calcul des énergies - On regarde où est-ce qu'elles se croisent pour déterminer le rayon viriel (méthode 2)
-Epp = -(3 * G * Masse ** 2) / (5 * R_final)
-Ec = Ecin(v)
-Ectest = Ecin(Vitesse2)
-Epvir = (3 * G * Masse ** 2) / (5 * Rvir)
+Epp = energie_pot(R_final)
+Ec = energie_cin(v)
+Ectest = energie_cin(v_num)
+Epvir = energie_pot(Rvir)
 
 surd_viriel_finale = 1 + delta_vir * (a(teff) / a(tvir1)) ** 3
 print("Surdensité viriel finale :", surd_viriel_finale)
@@ -138,18 +107,18 @@ if PLOT:
 
     plt.figure()
     # plt.plot(tmax,Rmax,'x',label="Rayon max")
-    plt.plot(tvir1 / milliard_annee, Rvir, 'x', label="Rayon Viriel")
-    plt.plot(tvir1 / milliard_annee, delta_vir, "+", label="Surdensité viriel")
+    plt.plot(tvir1, Rvir, 'x', label="Rayon Viriel")
+    plt.plot(tvir1, delta_vir, "+", label="Surdensité viriel")
     plt.plot(ttab, R_final, label="Rayon")
     plt.plot(ttab, delta_final, label="Surdensité")
-    plt.plot(tmax / milliard_annee, Rmax, 'x', label="Rayon max")
+    plt.plot(tmax, Rmax, 'x', label="Rayon max")
     plt.xscale('log')
     plt.yscale('log')
     plt.legend()
 
     plt.figure()
     plt.plot(ttab, abs(v), label="Vitesse théorique")
-    plt.plot(ttab, abs(Vitesse2), label="Vitesse numérique")
+    plt.plot(ttab, abs(v_num), label="Vitesse numérique")
     plt.xscale('log')
     plt.yscale('log')
     plt.legend()
