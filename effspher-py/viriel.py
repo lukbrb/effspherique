40977a9751dcdc3a2f1b534f4e85edc694d7013c
lukbrb <lucasbarbr@outlook.com>
2023-08-01 21:15:27 +0200

    clean viriel

--- effspher-py/viriel.py
+++ effspher-py/viriel.py
@@ -1,126 +1,103 @@
+"""
+Calcul de la surdensité viriel.
+
+1. Calculer R tel que 2Ec + Epot = 0 ou R = Rmax/2
+2. Calculer le temps auquel ce rayon est atteint
+3. Calculer à quel delta cela correspond
+4. Faire évoluer delta selon delta = delta x (a(teff) / a(tvir)) ** 3
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-from cosmofunc import rho_m, G, a, H, milliard_annee, surd_mini, eq_diff, tf, ti
+from cosmofunc import H, G, surd_mini, ti, eq_diff, tf, milliard_annee, rho_m, rho, a
 from solvers import rk4
 
 PLOT = True
+
 tf /= milliard_annee
 ti /= milliard_annee
 
-init = (4 * surd_mini, 4 * surd_mini * H(ti), ti)
-delta, p, ttab = rk4(init, eq_diff, tf, dt=1e-5, max_density=3 * 1e4)
+masse = 1e16
+delta, ddelta, ttab = rk4(4 * surd_mini, ti, eq_diff, tf, dt=1e-5, max_density=3 * 1e4)
 
-teff = ttab[-1]
-Masse = 1e16  # N'influe pas sur le résultat final de la surdensité
-
-
-# Fonction pour déterminer t lorsque Rviriel est atteint
-def t_viriel(borne_min, borne_max, tolerance, r, r_vir):
-    for i in r[borne_min:borne_max]:
-        inter = abs(i - r_vir)
-        t = ttab[borne_min]
-        while inter > tolerance:
-            milieu = (borne_min + borne_max) / 2
-            if i > r_vir:
-                borne_min = milieu
-            else:
-                borne_max = milieu
-            inter = borne_max - borne_min
-            t += 1
-        return t
+R = ((3 * masse) / (4 * np.pi * rho_m(ttab) * (1 + delta))) ** (1 / 3)
 
 
-# Fonction qui fait évoluer la surdensité après la surdensité viriel
-def surdensite(_delta, index):
-    _delta[index:] = rho_vir / rho_m(ttab[index:]) - 1
-    return _delta
+def vitesse(r):
+    """ Vitesse d'évolution du rayon de la sphère, v = dR/dt"""
+    return H(ttab) * r - (r * ddelta) / (3 * (1 + delta))
 
 
-def vitesse(r_final):
-    r = r_final
-    _v = H(ttab) * r * (1 - (p * r ** 3) / (9 * Masse * G * ttab))
-    v_test = H(ttab) * r - (r * p) / (3 * (1 + delta))
-    return _v, v_test
+def energie_cin(r):
+    v = vitesse(r)
+    return (3 * masse * v**2) / 10
 
 
-def Vitesse_alter(r, dt=1e-5):  # VITESSE CALCULEE DIRECTEMENT A PARTIR DE R ET dt
-    dr = np.diff(r)
-    _v = dr / dt
-    return np.pad(_v, (0, 1), mode='constant')  # numpy pad pour avoir len(v) == len(r)
+def energie_pot(r):
+    return - (3 * G * masse**2) / (5 * r)
 
 
-def energie_cin(_v):
-    return (3 * Masse * _v ** 2) / 10
+def find_rta(r, t, energie=False):
+    """ Fonction pour calculer le rayon de volte-face (ou donc rayon max)
+        et le temps associé.
+        Si energie=True, trouve Rta via Ecin = 0
+    """
+    if not energie:
+        imax = r.argmax()
+        return r[imax], t[imax]
+
+    # trouver Ecin = 0 <=> v = 0
+    imax = np.abs(vitesse(r)).argmin()
+    return r[imax], t[imax]
 
 
-def energie_pot(r_vir):
-    return -(3 * G * Masse ** 2) / (5 * r_vir)
-# Calcul du rayon viriel en prenant la moitié de son rayon max (méthode 1)
+def find_rvir(r, t, energie=False):
+    """ Fonction pour calculer le rayon viriel défini par Rta/2
+        et le temps associé.
+        Si energie=True, Trouve R via 2Ecin + Epot = 0, soit v**2 = GM/r
+    """
+    if not energie:
+        raise NotImplementedError("Actuellement impossible de trouver tvir sans la méthode énergie")
+        # rta = find_rta(r, t, energie=False)[0]
+        # ivir = np.abs(r - rta/2).argmin()
+        # return r[ivir], t[ivir]
+
+    vvir = -np.sqrt(G * masse / r)  # on veut la racine négative puisque v < 0 lorsque rayon se rétracte
+    ivir = np.abs(vitesse(r) - vvir).argmin()
+    return r[ivir], t[ivir]
 
 
-R = ((3 * Masse) / (4 * np.pi * rho_m(ttab) * (1 + delta))) ** (1 / 3)
-Rmax, imax = R.max(), R.argmax()
-tmax = ttab[imax]
-Rvir = Rmax / 2
+Rta, tmax = find_rta(R, ttab, energie=False)
+Rvir, tvir = find_rvir(R, ttab, energie=True)
+delta_vir, _, _ = rk4(4 * surd_mini, ti, eq_diff, t_max=tvir, dt=1e-5, max_density=3 * 1e4)
+delta_vir = delta_vir[-1]
+delta_vir2 = rho(Rvir, masse) / rho_m(tvir) - 1
+print(delta_vir, delta_vir2, (18 * np.pi**2) / ((a(ttab[-1]) / a(tvir)) ** 3))
+delta_fin = 1 + delta_vir * (a(ttab[-1]) / a(tvir)) ** 3
+delta_fin2 = 1 + delta_vir2 * (a(ttab[-1]) / a(tvir)) ** 3
 
-print("Rayon maximum:", Rmax, "atteint à:", tmax)
-print("Le rayon viriel est:", Rvir)
-# On ajuste les valeurs de la surdensité une fois R_vir atteint
-
-tvir1 = t_viriel(0, len(delta), 1e-6, R, Rvir)
-rho_vir = (3 * Masse) / (4 * np.pi * Rvir ** 3)  # Densité volumique de virialisation
-delta_vir = rho_vir / rho_m(tvir1) - 1  # Surdensité viriel
-delta[delta > delta_vir] = delta_vir  # Obsolète
-indice = delta.argmax()
-print("Indice de la virialisation :", indice)
-print("Temps viriel calculé avec la fonction", tvir1)
-
-# Rayon et surdensité finaux
-delta_final = surdensite(delta, indice)
-R_final = ((3 * Masse) / (4 * np.pi * rho_m(ttab) * (1 + delta_final))) ** (1 / 3)
-
-# Calcul des vitesses via méthodes numériques et formule analytique
-v, vtest = vitesse(R_final)
-v_num = Vitesse_alter(R_final)
-
-# Calcul des énergies - On regarde où est-ce qu'elles se croisent pour déterminer le rayon viriel (méthode 2)
-Epp = energie_pot(R_final)
-Ec = energie_cin(v)
-Ectest = energie_cin(v_num)
-Epvir = energie_pot(Rvir)
-
-surd_viriel_finale = 1 + delta_vir * (a(teff) / a(tvir1)) ** 3
-print("Surdensité viriel finale :", surd_viriel_finale)
-print("Surdensité viriel finale attendue:", 18 * np.pi ** 2)
-# print(delta_final[410] + 1)  # Ici l'indice n'est valable que pour une certaine valeur de N (pour N=5000)
+print(delta_fin, delta_fin2, 18 * np.pi**2)
 
 if PLOT:
     plt.figure()
-    plt.plot(ttab, 2 * Ectest, label="Energie cinétique en fonction de t")
-    plt.plot(ttab, abs(Epp), label="Energie potentielle en fonction de t")
-    plt.plot(tvir1, Epvir, "x", label='Energie potentielle au rayon viriel')
-
-    plt.xscale('log')
-    plt.yscale('log')
+    plt.title("Évolution du rayon")
+    plt.plot(ttab, R, 'r', label="Rayon")
+    plt.plot(tmax, Rta, 'x', label="Rayon max")
+    plt.plot(tvir, Rvir, 'x', label="Rayon viriel")
+    plt.xlabel('Temps (Gyr)')
+    plt.ylabel('R(t)')
+    # plt.yscale('log')
     plt.legend()
 
+    drdt = vitesse(R)
     plt.figure()
-    # plt.plot(tmax,Rmax,'x',label="Rayon max")
-    plt.plot(tvir1, Rvir, 'x', label="Rayon Viriel")
-    plt.plot(tvir1, delta_vir, "+", label="Surdensité viriel")
-    plt.plot(ttab, R_final, label="Rayon")
-    plt.plot(ttab, delta_final, label="Surdensité")
-    plt.plot(tmax, Rmax, 'x', label="Rayon max")
-    plt.xscale('log')
-    plt.yscale('log')
+    plt.title("Profil des vitesses")
+    plt.plot(ttab, drdt, '--k', label="Vitesse")
+    plt.vlines(tmax, drdt.min(), drdt.max(), colors='red', label="Vitesse au rayon max")
+    plt.vlines(tvir, drdt.min(), drdt.max(), colors='orange', label="Vitesse au rayon viriel")
+    plt.xlabel('Temps (Gyr)')
+    plt.ylabel('R(t)')
+    # plt.yscale('log')
     plt.legend()
-
-    plt.figure()
-    plt.plot(ttab, abs(v), label="Vitesse théorique")
-    plt.plot(ttab, abs(v_num), label="Vitesse numérique")
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.legend()
-
     plt.show()
