ad1c453a50610652e68898b0be0e46cbe492b371
lukbrb <lucasbarbr@outlook.com>
2023-08-04 18:36:01 +0200

    create class Surdensite to avoid global parameters

--- effspher-py/viriel.py
+++ effspher-py/viriel.py
@@ -8,103 +8,75 @@ Calcul de la surdensité viriel.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 
-from cosmofunc import H, G, surd_mini, ti, eq_diff, tf, milliard_annee, rho_m, rho, a
+from cosmofunc import H, G, eq_diff, rho_m, rho, a
 from solvers import integrateur
 
-PLOT = True
 
-tf /= milliard_annee
-ti /= milliard_annee
-dt = 1e-5
-masse = 1e16
-delta, ddelta, ttab = integrateur(4 * surd_mini, ti, eq_diff, tf, dt=dt, max_density=4 * 1e7, array=True)
+class Surdensite:
+    def __init__(self, di, ti, equation, tf, dt=1e-5, max_density=4 * 1e7, masse=1e16):
+        self.di = di
+        self.ti = ti
+        self.equation = equation
+        self.tf = tf
+        self.dt = dt
+        self.max_density = max_density
+        solution = integrateur(di, ti, eq_diff, tf, dt=dt, max_density=max_density, array=True)
+        self. delta, self.ddelta, self.ttab = solution
+        self.masse = masse
+        self.R = ((3 * masse) / (4 * np.pi * rho_m(self.ttab) * (1 + self.delta))) ** (1 / 3)
+        self.tvir = None
+        self.rvir = None
 
-R = ((3 * masse) / (4 * np.pi * rho_m(ttab) * (1 + delta))) ** (1 / 3)
+    def vitesse(self, r):
+        """ Vitesse d'évolution du rayon de la sphère, v = dR/dt"""
+        return H(self.ttab) * r - (r * self.ddelta) / (3 * (1 + self.delta))
 
+    def energie_cin(self, r):
+        v = self.vitesse(r)
+        return (3 * self.masse * v**2) / 10
 
-def vitesse(r):
-    """ Vitesse d'évolution du rayon de la sphère, v = dR/dt"""
-    return H(ttab) * r - (r * ddelta) / (3 * (1 + delta))
+    def energie_pot(self, r):
+        return - (3 * G * self.masse**2) / (5 * r)
 
+    def find_rta(self, energie=False):
+        """ Fonction pour calculer le rayon de volte-face (ou donc rayon max)
+            et le temps associé.
+            Si energie=True, trouve Rta via Ecin = 0
+        """
+        if not energie:
+            imax = self.R.argmax()
+            return self.R[imax], self.ttab[imax]
 
-def energie_cin(r):
-    v = vitesse(r)
-    return (3 * masse * v**2) / 10
+        # trouver Ecin = 0 <=> v = 0
+        imax = np.abs(self.vitesse(self.R)).argmin()
+        return self.R[imax], self.ttab[imax]
 
+    def find_rvir(self, energie=True):
+        """ Fonction pour calculer le rayon viriel défini par Rta/2
+            et le temps associé.
+            Si energie=True, Trouve R via 2Ecin + Epot = 0, soit v**2 = GM/r
+        """
+        if not energie:
+            raise NotImplementedError("Actuellement impossible de trouver tvir sans la méthode énergie")
+            # rta = find_rta(r, t, energie=False)[0]
+            # ivir = np.abs(r - rta/2).argmin()
+            # return r[ivir], t[ivir]
 
-def energie_pot(r):
-    return - (3 * G * masse**2) / (5 * r)
+        vvir = -np.sqrt(G * self.masse / self.R)  # on veut la racine négative puisque v < 0 lorsque rayon se rétracte
+        ivir = np.abs(self.vitesse(self.R) - vvir).argmin()
+        return self.R[ivir], self.ttab[ivir]
 
+    def surd_vir(self, _rho=False):
+        self.rvir, self.tvir = self.find_rvir()
+        if _rho:
+            return rho(self.rvir, self.masse) / rho_m(self.tvir) - 1
+        sol = integrateur(self.di, self.ti, self.equation, t_max=self.tvir, dt=self.dt, max_density=self.max_density)
+        return sol[0]
 
-def find_rta(r, t, energie=False):
-    """ Fonction pour calculer le rayon de volte-face (ou donc rayon max)
-        et le temps associé.
-        Si energie=True, trouve Rta via Ecin = 0
-    """
-    if not energie:
-        imax = r.argmax()
-        return r[imax], t[imax]
-
-    # trouver Ecin = 0 <=> v = 0
-    imax = np.abs(vitesse(r)).argmin()
-    return r[imax], t[imax]
-
-
-def find_rvir(r, t, energie=False):
-    """ Fonction pour calculer le rayon viriel défini par Rta/2
-        et le temps associé.
-        Si energie=True, Trouve R via 2Ecin + Epot = 0, soit v**2 = GM/r
-    """
-    if not energie:
-        raise NotImplementedError("Actuellement impossible de trouver tvir sans la méthode énergie")
-        # rta = find_rta(r, t, energie=False)[0]
-        # ivir = np.abs(r - rta/2).argmin()
-        # return r[ivir], t[ivir]
-
-    vvir = -np.sqrt(G * masse / r)  # on veut la racine négative puisque v < 0 lorsque rayon se rétracte
-    ivir = np.abs(vitesse(r) - vvir).argmin()
-    return r[ivir], t[ivir]
-
-
-Rta, tmax = find_rta(R, ttab, energie=True)
-Rvir, tvir = find_rvir(R, ttab, energie=True)
-delta_vir, _, _ = integrateur(4 * surd_mini, ti, eq_diff, t_max=tvir, dt=dt, max_density=4 * 1e7)
-
-delta_vir2 = rho(Rvir, masse) / rho_m(tvir) - 1
-print(delta_vir, delta_vir2, (18 * np.pi**2) / ((a(ttab[-1]) / a(tvir)) ** 3))
-delta_fin = 1 + delta_vir * (a(ttab[-1]) / a(tvir)) ** 3
-delta_fin2 = 1 + delta_vir2 * (a(ttab[-1]) / a(tvir)) ** 3
-
-print(delta_fin, delta_fin2, 18 * np.pi**2)
-
-if PLOT:
-    plt.figure()
-    plt.title("Évolution du rayon")
-    plt.plot(ttab, R, 'r', label="Rayon")
-    plt.plot(tmax, Rta, 'x', label="Rayon max")
-    plt.plot(tvir, Rvir, 'x', label="Rayon viriel")
-    plt.xlabel('Temps (Gyr)')
-    plt.ylabel('R(t)')
-    # plt.yscale('log')
-    plt.legend()
-
-    drdt = vitesse(R)
-    plt.figure()
-    plt.title("Profil des vitesses")
-    plt.plot(ttab, drdt, '--k', label="Vitesse")
-    plt.vlines(tmax, drdt.min(), drdt.max(), colors='red', label="Vitesse au rayon max")
-    plt.vlines(tvir, drdt.min(), drdt.max(), colors='orange', label="Vitesse au rayon viriel")
-    plt.xlabel('Temps (Gyr)')
-    plt.ylabel('R(t)')
-    # plt.yscale('log')
-    plt.legend()
-
-    plt.figure()
-    plt.title("Évolution de la surdensité")
-    plt.plot(ttab, delta, '--k')
-    plt.xlabel('Temps (Gyr)')
-    plt.ylabel('Surdensité $\delta$')
-    plt.yscale('log')
-    plt.show()
+    def surd_finale(self):
+        dvir = self.surd_vir()
+        if self.tvir:
+            return 1 + dvir * (a(self.ttab[-1]) / a(self.tvir)) ** 3
+        else:
+            return None
