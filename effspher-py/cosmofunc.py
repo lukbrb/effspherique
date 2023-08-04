5d1b1b2cb637e4721769de60814d390adb40540a
lukbrb <lucasbarbr@outlook.com>
2023-08-04 18:36:35 +0200

    switch time unit to Gyr by default

--- effspher-py/cosmofunc.py
+++ effspher-py/cosmofunc.py
@@ -7,18 +7,14 @@ G = 6.6742 * 1e-11  # Constante de gravitation
 rho_crit = (3 * H0 ** 2) / (8 * np.pi * G)  # Densité critique
 sig = 1  # Densité de l'univers
 milliard_annee = 3600 * 24 * 365 * 1e9
-age_univ = 2 / (3 * H0)  # L'âge de l'univers
+age_univ = (2 / (3 * H0)) / milliard_annee  # L'âge de l'univers
 surd_mini = 0.0017104343414306644  # Surdensité mini pour que teff <= age_univers
 # CONDITIONS INITIALES
 # On veut ti très petit devant 1/H0, or 1/H0= 4.4e17s (=13 952 308 472 ans)
 # On peut donc prendre ti=300000 ans par exemple
 
-ti = 300000 * 365 * 24 * 3600
-tf = 1 / H0
-N = 5000
-ttab = np.linspace(ti, tf, N)
-dt = (tf - ti) / N
-ttab1 = ttab / milliard_annee
+ti = (300000 * 365 * 24 * 3600) / milliard_annee
+tf = (1 / H0) / milliard_annee
 
 
 def H(t):
