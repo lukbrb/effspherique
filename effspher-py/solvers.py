c9f87c46d0c803cb5d63d5622d26de5dfb30875f
lukbrb <lucasbarbr@outlook.com>
2023-08-05 13:27:15 +0200

    change appelle fonction, ajout rk2 sans tableaux

--- effspher-py/solvers.py
+++ effspher-py/solvers.py
@@ -19,8 +19,36 @@ def euler(init_conds, function, t_max, dt=1e-5, max_density=np.inf):
     ttab = []
     delta, p, t = init_conds
     while t <= t_max and delta <= max_density:
-        step = function([delta, p], t) * dt
+        step = function(delta, p, t) * dt
         p += step
+        delta += p * dt
+
+        t += dt
+        results.append(delta)
+        ttab.append(t)
+
+    return results, ttab
+
+
+def rk2_array(init_conds, function, t_max, dt=1e-5, max_density=np.inf):
+    """
+        Fonction qui calcule l'algorithme de Runge-Kutta 2.
+        -------------
+        Arguments:
+            - init_conds: tuple (delta_0, p_0, t_0) des conditions initiales
+            - function : fonction directrice du système. Supposée ici f(x,t)
+            - t_max : Borne supérieure du domaine temporel (d'intégration)
+            - dt : le pas d'intégration
+            - max_density : valeur maximale autorisée pour la densité
+        :return delta_f, t_f
+        """
+    results = []
+    ttab = []
+    delta, p, t = init_conds
+    while t <= t_max and delta <= max_density:
+        k1 = function(delta, p, t)
+        k2 = function(delta, p + dt * k1, t + dt)
+        p += 0.5 * dt * (k1 + k2)
         delta += p * dt
 
         t += dt
@@ -42,20 +70,17 @@ def rk2(init_conds, function, t_max, dt=1e-5, max_density=np.inf):
             - max_density : valeur maximale autorisée pour la densité
         :return delta_f, t_f
         """
-    results = []
-    ttab = []
+
     delta, p, t = init_conds
     while t <= t_max and delta <= max_density:
-        k1 = function([delta, p], t)
-        k2 = function([delta, p + dt * k1], t + dt)
+        k1 = function(delta, p, t)
+        k2 = function(delta, p + dt * k1, t + dt)
         p += 0.5 * dt * (k1 + k2)
         delta += p * dt
 
         t += dt
-        results.append(delta)
-        ttab.append(t)
 
-    return results, ttab
+    return delta, p, t
 
 
 def rk4_array(delta_i, _ti, function, t_max, dt=1e-5, max_density=np.inf):
@@ -77,10 +102,10 @@ def rk4_array(delta_i, _ti, function, t_max, dt=1e-5, max_density=np.inf):
     p = delta_i * H(_ti)
 
     while t <= t_max and delta <= max_density:
-        k1 = function([delta, p], t)
-        k2 = function([delta, p + (dt * k1) / 2], t + dt / 2)
-        k3 = function([delta, p + (dt * k2) / 2], t + dt / 2)
-        k4 = function([delta, p + (dt * k3) / 2], t + dt)
+        k1 = function(delta, p, t)
+        k2 = function(delta, p + (dt * k1) / 2, t + dt / 2)
+        k3 = function(delta, p + (dt * k2) / 2, t + dt / 2)
+        k4 = function(delta, p + (dt * k3) / 2, t + dt)
         p += (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
         delta += p * dt
 
@@ -109,10 +134,10 @@ def rk4(delta_i, _ti, function, t_max, dt=1e-5, max_density=1e4):
     p = delta_i * H(_ti)
 
     while t <= t_max and delta <= max_density:
-        k1 = function([delta, p], t)
-        k2 = function([delta, p + (dt * k1) / 2], t + dt / 2)
-        k3 = function([delta, p + (dt * k2) / 2], t + dt / 2)
-        k4 = function([delta, p + (dt * k3) / 2], t + dt)
+        k1 = function(delta, p, t)
+        k2 = function(delta, p + (dt * k1) / 2, t + dt / 2)
+        k3 = function(delta, p + (dt * k2) / 2, t + dt / 2)
+        k4 = function(delta, p + (dt * k3) / 2, t + dt)
         p += (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
         delta += p * dt
 
