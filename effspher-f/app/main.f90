ec7081c1355ef6dc212f96f940cd24919a7f1f68
lukbrb <lucasbarbr@outlook.com>
2023-09-02 20:26:13 +0200

    finalisation avec surd_vf, surd_f

--- effspher-f/app/main.f90
+++ effspher-f/app/main.f90
@@ -1,23 +1,33 @@
 program main
   use solvers, only: rk2, rk4, euler, rk4_write
-  use cosmofunc, only: di, ti, t_max, age_univers
+  use cosmofunc, only: di, ti, t_max, age_univers, PI
   use paramtune, only: cond_init, iterate_on_param
-  use viriel, only: surd_vir, surd_finale
+  use viriel, only: surd_vir, surd_finale, surd_ta
   
   implicit none
-
+  ! Régime non-linéaire
+  ! ----------
+  ! Surdensité volte-face: 4.552053651856895
+  ! Surdensité volte-face théorique: 4.551652475612764
+  ! Différence relative:  0.008814%
+  ! ----------
+  ! Surdensité viriel finale: 177.6523156109369
+  ! Surdensité viriel théorique: 177.65287921960845
+  ! Différence relative:  0.000317%
+  ! ----------
+  
   real :: dt = 1e-5
   real :: max_density = 1e4
   real :: surdensite_mini
-  real, dimension(2) :: resulte, result2, result4, surdensite_vir
-  real, dimension(1) :: surd_fin
-
-  resulte = euler(4. * di, ti, t_max, dt, max_density)
-  result2 = rk2(4. * di, ti, t_max, dt, max_density)
-  result4 = rk4_write(4. * di, ti, t_max, dt, max_density)
-  print *, 'Résultat Euler explicit :', resulte
-  print *, 'Résultat RK2 :', result2
-  print *, 'Résultat RK4 :', result4
+  real, dimension(2) :: resulte, result2, result4
+  real, dimension(1) :: surd_fin, surdensite_vir
+  real :: sta
+  ! resulte = euler(4. * di, ti, t_max, dt, max_density)
+  ! result2 = rk2(4. * di, ti, t_max, dt, max_density)
+  ! result4 = rk4_write(4. * di, ti, t_max, dt, max_density)
+  ! print *, 'Résultat Euler explicit :', resulte
+  ! print *, 'Résultat RK2 :', result2
+  ! print *, 'Résultat RK4 :', result4
 
   ! ----------------------------
   ! Itération sur les paramètres
@@ -25,9 +35,14 @@ program main
 
   
   surdensite_mini = cond_init(1e-4, 1e-2, 1e-7, 1e4, age_univers)
-  print *, 'Surdensité minimum pour t_eff=âge univers:', surdensite_mini
+  write(*, '(A, F12.6)') 'Surdensité minimum pour t_eff=âge univers:', surdensite_mini
   surdensite_vir = surd_vir()
-  print *, surdensite_vir
+  write(*, '(A, F12.6)') 'Surdensité viriel calculée:', surdensite_vir
+
+  sta = surd_ta()
+  write(*, '(A, F12.6)') 'Surdensité volte-face calculée:', sta
+  write(*, '(A, F12.6)') 'Surdensité volte-face théorique:', (9./16.) * PI**2 - 1.
   surd_fin = surd_finale()
-  write(*, *), 'surd_fin', surd_fin
+  write(*, '(A, F12.6)') 'Surdensité finale calculée:', surd_fin
+  write(*, '(A, F12.6)') 'Surdensité finale théorique:', 18. * PI**2
 end program main
--- effspher-f/src/viriel.f90
+++ effspher-f/src/viriel.f90
@@ -1,59 +1,18 @@
 module viriel
     use cosmofunc, only: Hvec, G, di, ti, rho_m, masse, PI, exp_param
-
+    use io, only: read_data
     use solvers, only: rk4
     implicit none
 
     private
     character(256), parameter :: filename = 'results/rk4-results.dat'
-    public :: surd_vir, surd_finale
+    public :: surd_vir, surd_finale, surd_ta
 contains
-
-    subroutine read_t_and_delta(t, ddelta, delta)
-        real, dimension(:), allocatable, intent(out) :: t, ddelta, delta
-        character(len=512) :: msg
-        integer :: num_rows, i
-        integer :: io, status
-        logical :: exists
-        
-        inquire(file=filename, exist=exists)
-        if (exists) then
-            open(unit=io, file=filename, status='old', action='read', iostat=status, iomsg=msg)
-            if (status /= 0) then
-                write(*,*) "Erreur lors de l'ouverture du fichier ", trim(filename), ':', trim(msg)
-                stop
-            end if
-        end if
-        
-        ! Compter le nombre de lignes dans le fichier
-        num_rows = 0
-        do
-            read(io, *, iostat=status)
-            if (status /= 0) exit
-            num_rows = num_rows + 1
-        end do
-    
-        ! Allouer de la mémoire pour les tableaux
-        allocate(t(num_rows))
-        allocate(ddelta(num_rows))
-        allocate(delta(num_rows))
-    
-        ! Revenir au début du fichier
-        rewind(io)
-        ! Lire les données dans les tableaux
-        do i = 1, num_rows
-            read(io, *) t(i), ddelta(i), delta(i)
-        end do
-
-        close(io)
-        
-        end subroutine read_t_and_delta
 
     function vitesse(t, r, ddelta, delta) result(v)
         real, dimension(:), allocatable, intent(in) :: t, r, ddelta, delta
-        real, dimension(:), allocatable:: v
+        real, dimension(:), allocatable :: v
         allocate(v(size(t)))
-
         v = Hvec(t) * r - (r * ddelta) / (3 * (1 + delta))
     end function vitesse
 
@@ -70,44 +29,71 @@ contains
     !     ep = - (3 * G * masse ** 2) / (5 * r)
     ! end function energie_pot
 
+    function find_rta(t, r, ddelta, delta, energie) result(rta_and_tta)
+        logical, intent(in) :: energie
+        real, allocatable, dimension(:), intent(in) :: t, r, ddelta, delta
+        real, dimension(2) :: rta_and_tta
+        integer, dimension(1) :: imax
+        if (energie) then
+            imax = minloc(abs(vitesse(t, r, ddelta, delta)))
+        else
+            imax = maxloc(r)
+        end if
+        rta_and_tta = [r(imax(1)), t(imax(1))]
+    end function find_rta
+
+    function surd_ta() result(sta)
+        real, dimension(:), allocatable:: t, ddelta, delta, r
+        real, dimension(2) :: rta_et_tta, sol
+        real :: sta
+        call read_data(t, ddelta, delta)
+        allocate(r(size(t)))
+        r = ((3 * masse) / (4 * PI * rho_m(t) * (1 + delta))) ** (1. / 3.)
+        rta_et_tta = find_rta(t, r, ddelta, delta, energie=.true.)
+        deallocate(ddelta)
+        deallocate(delta)
+        sol = rk4(4. * di, ti, t_max=rta_et_tta(2), dt=1E-5, max_density=1E4)
+        sta = sol(2)
+    end function surd_ta
+
+
     subroutine find_rvir(t_and_r, t, r, ddelta, delta)
         real, dimension(:), allocatable, intent(in) :: t, r, ddelta, delta
-        real, dimension(:), allocatable :: vvir, vdiff
+        real, dimension(:), allocatable :: vvir
         real, dimension(2) :: t_and_r
         real :: rvir, tvir
         integer, dimension(1) :: index_vir
 
         allocate(vvir(size(t)))
-        allocate(vdiff(size(t)))
         vvir = -sqrt(G * masse / r)
-        vdiff = vitesse(t, r, ddelta, delta) - vvir
-        index_vir = minloc(abs(vdiff))
+        index_vir = minloc(abs(vitesse(t, r, ddelta, delta) - vvir))
         deallocate(vvir)
-        deallocate(vdiff)
+
         tvir = t(index_vir(1))
         rvir = r(index_vir(1))
         t_and_r = [tvir, rvir]        
     end subroutine find_rvir
 
-    function surd_vir() result(sol)
+    function surd_vir() result(svir)
         real, dimension(:), allocatable:: t, r, ddelta, delta
-        real :: tvir
         real, dimension(2) :: t_and_r, sol
-        call read_t_and_delta(t, ddelta, delta)
+        real :: svir
+        call read_data(t, ddelta, delta)
         ! write(*, '(5F10.4)') delta
         allocate(r(size(t)))
         r = ((3 * masse) / (4 * PI * rho_m(t) * (1 + delta))) ** (1. / 3.)
         call find_rvir(t_and_r, t, r, ddelta, delta)
-        tvir = t_and_r(1)
-        print *, 'tvir=', tvir
-        sol = rk4(4. * di, ti, t_max=tvir, dt=1E-5, max_density=1E4)
+        deallocate(ddelta)
+        deallocate(delta)
+        sol = rk4(4. * di, ti, t_max=t_and_r(1), dt=1E-5, max_density=1E4)
+        svir = sol(2)
     end function surd_vir
 
     function surd_finale() result(dfinal)
         real, dimension(:), allocatable:: t, ddelta, delta
         real, dimension(2) :: tvir_dvir
         real, dimension(1) :: tfin, tvir, dfinal
-        call read_t_and_delta(t, ddelta, delta)
+        call read_data(t, ddelta, delta)
         deallocate(ddelta)
         deallocate(delta)
         tfin(1) = t(size(t))
